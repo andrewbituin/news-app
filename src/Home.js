51b5f00bb1644d98717d97ca3e4146b67b6502fc
andrewbituin <fandrewbituin@gmail.com>
2019-02-25 14:01:02 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Headlines from './components/Headlines';
+
+export default function Home(){
+    return (
+        <div>
+            <h1>News App</h1>
+            <Headlines />
+        </div>
+    )
+}
---
+++ src/components/Headlines.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Headlines.css";
+export default class Headlines extends React.Component {
+  state = {
+    articles: [],
+    currentArticle: 0
+  };
+
+  componentDidMount() {
+    fetch("https://newsapi.org/v2/top-headlines?country=us", {
+      method: "GET",
+      headers: {
+        "x-api-key": "4875586f6bc04b13bcde4967e8e2c20d"
+      }
+    })
+      .then(res => res.json())
+      .then(data => this.setState({ articles: data.articles }));
+  }
+  //Display only 6 headlines at a time
+  // On the click of button, rotate to next section of headlines
+  displayHeadlines = () => {
+    return this.state.articles.map(article => {
+      return (
+        <li className="headline">
+          <h3>{article.title}</h3>
+          <p>{article.author ? `author: ${article.author}` : ""}</p>
+          <p>{article.description}</p>
+        </li>
+      );
+    });
+  };
+  render() {
+    return (
+      <div>
+        <ul>{this.displayHeadlines()}</ul>
+        <button type="click">More Headlines</button>
+      </div>
+    );
+  }
+}
